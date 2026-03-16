3494197d75baa7236b2322314630356abc9ab890
Jorge Perez - M4 <jorge_alberto1997@hotmail.com>
2026-03-16 00:50:50 -0600

    Install mongoose and mongodb and connect to db

--- natours/server.js
+++ natours/server.js
@@ -1,8 +1,36 @@
-const dotenv = require('dotenv')
-dotenv.config({ path: './config.env' })
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose.connect(DB).then(() => {
+  console.log('DB connected successfully');
+});
 
 const app = require('./app');
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+// Models start with capital letters
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
