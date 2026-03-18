8ccefbbc10cb4232407113c4183ba5e349b506ac
Jorge Perez - M4 <jorge_alberto1997@hotmail.com>
2026-03-18 10:34:39 -0600

    Create script to load all data and to delete them

---
+++ natours/dev-data/data/import-dev-data.js
@@ -0,0 +1,53 @@
+const fs = require('node:fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    dbName: process.env.DATABASE_NAME,
+  })
+  .then(() => {
+    console.log('DB connected successfully');
+  });
+
+// Read json file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+// Import data into db
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+// Delete all documents in DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+//console.log(process.argv);
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
