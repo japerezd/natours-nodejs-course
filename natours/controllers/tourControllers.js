fd4e860067621af5ca5aac2f9ccf1abf02ab0065
Jorge Perez - M4 <jorge_alberto1997@hotmail.com>
2026-03-18 11:13:15 -0600

    Add filters

--- natours/controllers/tourControllers.js
+++ natours/controllers/tourControllers.js
@@ -17,7 +17,18 @@ const Tour = require('../models/tourModel');
 // 2. Route handlers
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const tours = await Tour.find(queryObj);
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
     res.status(200).json({
       requestedAt: req.requestTime,
       status: 'success',
