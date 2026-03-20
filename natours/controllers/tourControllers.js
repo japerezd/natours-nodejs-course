141eb1ac3157935cd5fde214cef430668130511a
Jorge Perez - M4 <jorge_alberto1997@hotmail.com>
2026-03-20 14:04:27 -0600

    Add built-in data validators

--- natours/controllers/tourControllers.js
+++ natours/controllers/tourControllers.js
@@ -82,10 +82,9 @@ exports.createTour = async (req, res) => {
       },
     });
   } catch (error) {
-    console.log('Error', error);
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent!',
+      message: error,
     });
   }
 };
--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -9,6 +9,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxLength: [40, 'A tour must have less or equal than 40 characters'],
+      minLength: [10, 'A tour must have more or equal than 10 characters'],
     },
     slug: String,
     duration: {
@@ -22,10 +24,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty must be one of these: easy, medium or difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating mus be above 1.0'],
+      max: [5, 'Rating mus be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
