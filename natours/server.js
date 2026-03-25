e687af869484fe95dabf7b7fc782eaffbcf42e7a
Jorge Perez - M4 <jorge_alberto1997@hotmail.com>
2026-03-25 14:47:03 -0600

    Handle unhandled rejections

--- natours/server.js
+++ natours/server.js
@@ -18,6 +18,15 @@ mongoose
 const app = require('./app');
 
 const port = process.env.port || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on ${port} port...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION. Shutting down...');
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
