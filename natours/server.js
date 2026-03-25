7abc6e4d7af22d7f15104a15b36682c6015aaa9f
Jorge Perez - M4 <jorge_alberto1997@hotmail.com>
2026-03-25 14:53:12 -0600

    Handle uncaught exception

--- natours/server.js
+++ natours/server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNHANDLED EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
@@ -23,8 +30,8 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('UNHANDLED REJECTION. Shutting down...');
+  console.log(err.name, err.message);
 
   server.close(() => {
     process.exit(1);
